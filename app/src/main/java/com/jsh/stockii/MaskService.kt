ab500120c31085086447e19cc6617ea423443ce6
jsh <ppm_it@naver.com>
2020-07-29 11:44:06 +0900

    Add Retrofit Test code

---
+++ app/src/main/java/com/jsh/stockii/MaskService.kt
@@ -0,0 +1,9 @@
+package com.jsh.stockii
+
+import retrofit2.Call
+import retrofit2.http.GET
+
+interface MaskService {
+    @GET("sample.json")
+    fun fetchStoreInfo(): Call<Store>
+}
--- app/src/test/java/com/jsh/stockii/ExampleUnitTest.kt
+++ app/src/test/java/com/jsh/stockii/ExampleUnitTest.kt
@@ -1,8 +1,11 @@
 package com.jsh.stockii
 
+import com.jsh.stockii.Consts.baseUrl
 import org.junit.Test
 
 import org.junit.Assert.*
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -13,5 +16,19 @@ class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun retrofit_test(){
+        val retrofit = Retrofit.Builder()
+            .baseUrl(baseUrl)
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+            .create(MaskService::class.java)
+            .fetchStoreInfo()
+
+        val storeInfo = retrofit.execute().body()
+
+        assertEquals(222, storeInfo?.count)
     }
 }
