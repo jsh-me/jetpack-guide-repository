df6b4d53113df3dc3e88d8b3eade97c668ee7c85
jsh <ppm_it@naver.com>
2020-07-29 11:20:27 +0900

    Add Entity Class

---
+++ app/src/main/java/com/jsh/stockii/Store.kt
@@ -0,0 +1,6 @@
+package com.jsh.stockii
+
+data class Store (
+    var count: Int,
+    var stores: List<StoreInfo>
+)
---
+++ app/src/main/java/com/jsh/stockii/StoreInfo.kt
@@ -0,0 +1,13 @@
+package com.jsh.stockii
+
+data class StoreInfo(
+    var addr: String,
+    var code: String,
+    var created_at: String,
+    var lat: Double,
+    var lng: Double,
+    var name: String,
+    var remain_stat: String,
+    var stock_at: String,
+    var type: String
+)
