aa80778f10764531954ef660ffd1ff7f818a76ef
jsh <ppm_it@naver.com>
2020-07-30 14:30:50 +0900

    Add Network connection

--- app/src/main/java/com/jsh/stockii/MainActivity.kt
+++ app/src/main/java/com/jsh/stockii/MainActivity.kt
@@ -2,7 +2,7 @@ package com.jsh.stockii
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Layout
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,17 +11,46 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.item_store.view.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 class MainActivity : AppCompatActivity() {
+    val TAG = MainActivity::class.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        recycler_view.adapter = StoreAdapter()
-        recycler_view.layoutManager = LinearLayoutManager(this)
+        val storeAdapter = StoreAdapter()
 
-        testRecyclerView()
+        recycler_view.apply {
+            adapter = storeAdapter
+            layoutManager = LinearLayoutManager(this@MainActivity)
+        }
+            //testRecyclerView()
+         Retrofit.Builder()
+                .baseUrl(Consts.baseUrl)
+                .addConverterFactory(MoshiConverterFactory.create())
+                .build()
+                .create(MaskService::class.java)
+                .fetchStoreInfo()
+                .enqueue(object: Callback<StoreInfo>{
+                    override fun onFailure(call: Call<StoreInfo>, t: Throwable) {
+                        Log.e(TAG, t.localizedMessage!!)
+                    }
+
+                    override fun onResponse(call: Call<StoreInfo>, response: Response<StoreInfo>) {
+                        val items = response.body()?.stores
+                        items?.let {
+                            storeAdapter.updateItems(it)
+                        }
+                    }
+                })
+        }
+
     }
 
     private fun testRecyclerView(){
@@ -33,7 +62,6 @@ class MainActivity : AppCompatActivity() {
 
         StoreAdapter().updateItems(testItems)
     }
-}
 
 class StoreAdapter: RecyclerView.Adapter<StoreAdapter.StoreViewHolder>() {
     private var _items = listOf<Store>()
